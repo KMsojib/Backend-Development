7a3b59a224e7a663c24d2d3877e51ec618813338
KMsojib <kawsarmahmud822@gmail.com>
2026-06-03 16:10:45 +0600

    updates

--- projects/todo-apps/todos/admin.py
+++ projects/todo-apps/todos/admin.py
@@ -4,19 +4,13 @@ from .models import TodoItem
 
 @admin.register(TodoItem)
 class TodoItemAdmin(admin.ModelAdmin):
-    # Show all columns in the main list table.
+    
     list_display = ('title', 'category', 'priority', 'due_date', 'is_completed', 'is_overdue')
-    
-    # links to click to open the edit page
     list_display_links = ('title',)
-    
-    # Sidebar filtering panel options
     list_filter = ('is_completed', 'priority', 'category')
     
-    # Search bar targets
     search_fields = ('title', 'description', 'category')
     
-    # Allows editing these values directly from the list page view without opening the task!
     list_editable = ('is_completed', 'priority')
     
     @admin.display(boolean=True, description="Overdue?")
