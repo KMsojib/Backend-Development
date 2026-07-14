3b3802b8c1b8dca0aef81f7478aaf50304150a21
KMsojib <kawsarmahmud822@gmail.com>
2026-07-14 19:46:36 +0600

    update

--- projects/Multi-Tenant Wallet APi/wallets/admin.py
+++ projects/Multi-Tenant Wallet APi/wallets/admin.py
@@ -20,6 +20,15 @@ class CustomerAdmin(TenantScopedAdminBase):
     search_fields = ('user__username', 'email')
     raw_id_fields = ('user',)  
 
+class TransactionInline(admin.TabularInline):
+    model = Transaction
+    extra = 0
+    can_delete = True
+    readonly_fields = ('tenant', 'type', 'amount', 'reference_id', 'created_at')
+    
+    def had_add_permission(self, request, obj=None):
+        return False
+
 @admin.register(Wallet)
 class WalletAdmin(TenantScopedAdminBase):
     list_display = ('id', 'tenant', 'customer', 'currency', 'current_balance', 'created_at')
@@ -27,6 +36,8 @@ class WalletAdmin(TenantScopedAdminBase):
     search_fields = ('customer__user__username', 'id')
     readonly_fields = ('current_balance',)
 
+    inlines = [TransactionInline]
+    
     def current_balance(self, obj):
         return obj.balance
     current_balance.short_description = 'Live Balance (Minor Units)'
@@ -39,6 +50,11 @@ class TransactionAdmin(TenantScopedAdminBase):
     search_fields = ('wallet__id', 'reference_id')
     readonly_fields = ('created_at',)
 
+    def has_add_permission(self, request):
+        return False
+    def has_delete_permission(self, request):
+        return False
+    
     def amount_display(self, obj):
         return f"{obj.amount} units"
     amount_display.short_description = 'Amount'
